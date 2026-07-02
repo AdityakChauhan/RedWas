2ebeb0766f123838f883e448091887c6249212f8
Aditya Chauhan <aditya.chauhan.ks@gmail.com>
2026-07-02 09:38:24 +0000

    Stage 5 -> Serializer implemented

---
+++ src/serializer/RespSerializer.cpp
@@ -0,0 +1,22 @@
+#include "RespSerializer.h"
+using namespace std;
+
+string RespSerializer::simpleString(const string &s) {
+    return "+" + s + "\r\n";
+}
+
+string RespSerializer::bulkString(const string &s) {
+    return "$"+to_string(s.length())+"\r\n"+s+"\r\n";
+}
+
+string RespSerializer::nullBulk() {
+    return "$-1\r\n";
+}
+
+string RespSerializer::integer(long long n) {
+    return ":"+to_string(n)+"\r\n";
+}
+
+string RespSerializer::error(const string &msg) {
+    return "-ERR "+msg+"\r\n";
+}
---
+++ src/serializer/RespSerializer.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+using namespace std;
+
+class RespSerializer {
+    public:
+        static string simpleString(const string &s);
+        static string bulkString(const string &s);
+        static string nullBulk();
+        static string integer(long long n);
+        static string error(const string &msg);
+};
